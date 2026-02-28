8e1dde89a35270b2394508390f06d6c777674ffb
chanathip_nuisee <chanathip.nuis@gmail.com>
2026-02-28 13:26:48 +0700

    Edit cleaning

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ import plotly.express as px
 df1 = pd.read_csv("goldstock_v1.csv")
 df2 = pd.read_csv("Crude_Oil_Data.csv")
 df3 = pd.read_csv("SP500.csv")
+
 # Data cleaning
-df1["Date"] = pd.to_datetime(df1["Date"]).dt.tz_localize(None)
-df2["Date"] = pd.to_datetime(df2["Date"]).dt.tz_localize(None)
-df3["Date"] = pd.to_datetime(df3["Date"]).dt.tz_localize(None)
+df1["Date"] = pd.to_datetime(df1["Date"], utc=True).dt.tz_localize(None)
+df2["Date"] = pd.to_datetime(df2["Date"], utc=True).dt.tz_localize(None)
+df3["Date"] = pd.to_datetime(df3["Date"], utc=True).dt.tz_localize(None)
+
 # Initialize the app
 app = Dash(__name__)
+
 
 # Run the app
 if __name__ == "__main__":
